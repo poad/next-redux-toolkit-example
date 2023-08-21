9d4ed29586243c0bbe4c518fb59b1918e914d756
Kenji Saito <ken-yo@mbr.nifty.com>
2023-08-21 18:12:20 +0900

    Bumps node modules

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,5 @@
 import React, { PropsWithChildren } from 'react';
-import { Sawarabi_Gothic } from '@next/font/google';
+import { Sawarabi_Gothic } from 'next/font/google';
 import {
   AppBar,
   Box,
